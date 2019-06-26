3f4ce62803c257d008be4fcd2124dfb7be4508c9
Kirandeep Kaur <741453@student.tor.loc>
2019-06-26 14:17:40 -0400

    2:17pm commit

---
+++ javascriptfile1.js
@@ -0,0 +1,9 @@
+function waitingFn(timeInMs){
+    const futureTime=Date.now()+timeInMs;
+    while(futureTime>Date.now()){
+        // waiting
+    }
+
+}
+waitingFn(3000);
+console.log("Function call has just ended");
